8932651df4391e34d13357ef8e32fc350678239b
sachin <sa2342@gmail.com>
2024-02-07 10:23:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+   <Router>
+    <Navbar/>
+    <Routes>
+
+      <Route path='/' element={<Home/>} />
+
+    </Routes>
+      <Footer/> 
+   </Router>
+  )
+}
+
+export default App
